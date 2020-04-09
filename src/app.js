54ca418b296a87653dde51fbdbd73a8c7573814e
Gabriel Pedrosa <gabrielmip@protonmail.com>
2020-04-09 07:19:43 -0300

    Implementing the requested routes.

--- src/app.js
+++ src/app.js
@@ -1,33 +1,74 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const {uuid} = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+
 const repositories = [];
 
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.send(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+
+  const newRepository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(newRepository);
+
+  return response.status(201).json(newRepository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const requestedId = request.params.id;
+  const {title, url, techs} = request.body;
+
+  const currentRepository = repositories.find(({id}) => id === requestedId);
+  if (currentRepository === undefined) {
+    return response.status(400).send();
+  }
+
+  const updatedRepository = Object.assign(currentRepository, {title, url, techs});
+
+  return response.json(updatedRepository);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const requestedId = request.params.id;
+
+  const currentRepositoryIndex = repositories.findIndex(({id}) => id === requestedId);
+  if (currentRepositoryIndex < 0) {
+    return response.status(400).send();
+  }
+
+  repositories.splice(currentRepositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const requestedId = request.params.id;
+
+  const currentRepository = repositories.find(({id}) => id === requestedId);
+  if (currentRepository === undefined) {
+    return response.status(400).send();
+  }
+
+  currentRepository.likes++;
+
+  return response.json(currentRepository);
 });
 
 module.exports = app;
